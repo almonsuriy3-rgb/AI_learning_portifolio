17f354113d9f253edc9fe77958125cdabd68290b
almonsuriy3-rgb <almonsuriy3@gmail.com>
2025-12-28 14:50:30 +0100

    Create day1_day2_python_basics.py

---
+++ day1_day2_python_basics.py
@@ -0,0 +1,11 @@
+# Day 1: Print statements
+print("Hello, world!")
+print("My name is Monsur")
+print("I am starting AI from zero")
+
+# Day 2: Variables and calculations
+study_hours = 4
+score = study_hours * 10
+
+print("Study hours:", study_hours)
+print("Predicted score:", score)
