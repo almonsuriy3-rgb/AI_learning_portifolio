9791e7b39b6e0a6d155846d147434623e473a51c
almonsuriy3-rgb <almonsuriy3@gmail.com>
2025-12-28 19:29:28 +0100

    Decision making and loop examples

---
+++ day3_day4_logic_and_loops.py
@@ -0,0 +1,18 @@
+# Day 3: If / Else (Decision Making)
+score = 62
+
+if score >= 70:
+    print("Grade A")
+elif score >= 50:
+    print("Grade B")
+else:
+    print("Grade C")
+
+# Day 4: Loops
+for i in range(1, 11):
+    print(i, "squared is", i * i)
+
+students = ["Ayo", "Bola", "Tunde"]
+
+for student in students:
+    print("Student name:", student)
